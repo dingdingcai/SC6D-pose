5141248e981847c9c161f25e8a68b573a6148785
DingdingCai <dingding.cai@tuni.fi>
2022-08-10 11:55:48 +0300

    update readme

--- training.sh
+++ training.sh
@@ -1,4 +1,5 @@
 PORT=9999
 NAME='tless'
 BATCHSIZE=64
-python SC6D_training.py  --ip_adress=127.0.0.1  --port=$PORT --nodes=1 --local_rank=0 --ngpus=2 --batchsize=$BATCHSIZE --dataset_name=$NAME
+MAX_EPOCHS=75
+python SC6D_training.py  --ip_adress=127.0.0.1  --port=$PORT --nodes=1 --local_rank=0 --ngpus=2 --epochs=$MAX_EPOCHS --batchsize=$BATCHSIZE --dataset_name=$NAME
